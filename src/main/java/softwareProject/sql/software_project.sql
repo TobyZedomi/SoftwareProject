2d395d43c191f1a41994fe68e91c6f8d1418e2f3
TobyZedomi <tobyzedo4@gmail.com>
2025-01-27 20:51:48 +0000

    database tables with properties for database

---
+++ src/main/java/softwareProject/sql/software_project.sql
@@ -0,0 +1,141 @@
+DROP
+DATABASE IF EXISTS software_project;
+CREATE
+DATABASE IF NOT EXISTS software_project;
+
+USE
+software_project;
+
+CREATE TABLE users
+(
+    username varchar(50)         NOT NULL,
+    email    varchar(255) UNIQUE NOT NULL,
+    password varchar(255)        NOT NULL,
+    address  varchar(255) 	 NOT NULL,
+    isAdmin boolean NOT NULL DEFAULT false,
+    PRIMARY KEY (username)
+);
+
+CREATE TABLE subscription
+(
+    subscription_id        INT AUTO_INCREMENT,
+    username     varchar(255) NOT NULL,
+    subscription_plan 	varchar(255) NOT NULL,
+    subscription_startDate datetime NOT NULL,
+    subscription_endDate  datetime NOT NULL,
+    price double NOT NULL,
+    PRIMARY KEY (subscription_id),
+    FOREIGN KEY (username) REFERENCES users (username) ON UPDATE CASCADE
+);
+
+
+CREATE TABLE genre
+(
+    genre_id INT AUTO_INCREMENT,
+    genre_name varchar(255),
+    PRIMARY KEY (genre_id)
+);
+
+
+CREATE TABLE movies
+(
+    movie_id INT AUTO_INCREMENT,
+    movie_name varchar(255) NOT NULL,
+    genre_id int(11) NOT NULL,
+    date_of_release datetime NOT NULL,
+    movie_length time NOT NULL,
+    movie_info varchar(255) NOT NULL,
+    where_to_watch varchar(255) NOT NULL,
+    movie_image varchar(255) NOT NULL,
+    PRIMARY KEY (movie_id),
+    FOREIGN KEY (genre_id) REFERENCES genre (genre_id) ON UPDATE CASCADE
+);
+
+CREATE TABLE review
+(
+    username varchar(255) NOT NULL,
+    movie_id int(11) NOT NULL,
+    rating double NOT NULL,
+    comment text,
+    PRIMARY KEY (username, movie_id),
+    FOREIGN KEY (username) REFERENCES users (username) ON UPDATE CASCADE,
+    FOREIGN KEY (movie_id) REFERENCES movies (movie_id) ON UPDATE CASCADE
+);
+
+
+CREATE TABLE favouriteList
+(
+    favourite_id INT AUTO_INCREMENT,
+    username varchar(255) NOT NULL,
+    movie_id int(11) NOT NULL,
+    PRIMARY KEY (favourite_id),
+    FOREIGN KEY (username) REFERENCES  users (username) ON UPDATE CASCADE,
+    FOREIGN KEY (movie_id) REFERENCES movies (movie_id) ON UPDATE CASCADE
+);
+
+
+
+create table friends
+(
+    friend1 varchar(10) not null,
+    friend2 varchar(10) not null,
+    PRIMARY KEY (friend1, friend2),
+    FOREIGN KEY (friend1) REFERENCES users(username) on delete cascade,
+    FOREIGN KEY (friend2) REFERENCES users(username) on delete cascade
+);
+
+
+DELIMITER |
+CREATE TRIGGER enforce_friendship_order BEFORE INSERT ON friends
+    FOR EACH ROW BEGIN
+    SET @lowerName := IF(NEW.friend1 < NEW.friend2, NEW.friend1, NEW.friend2);
+    SET @higherName := IF(NEW.friend1 > NEW.friend2, NEW.friend1, NEW.friend2);
+    SET NEW.friend1 = @lowerName;
+    SET NEW.friend2 = @higherName;
+END;
+|
+DELIMITER ;
+
+
+
+
+CREATE TABLE movieItem
+(
+    movieItem_id INT AUTO_INCREMENT,
+    movieItem_name varchar(255) NOT NULL,
+    movie_id int(11) NOT NULL,
+    list_price double NOT NULL,
+    stock int NOT NULL,
+    PRIMARY KEY (movieItem_id),
+    FOREIGN KEY (movie_id) REFERENCES movies (movie_id) ON UPDATE CASCADE
+);
+
+
+CREATE TABLE orderItems
+(
+    order_id INT AUTO_INCREMENT,
+    movieItem_id int(11),
+    quantity int (11),
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (movieItem_id) REFERENCES movieItem (movieItem_id) ON UPDATE CASCADE
+);
+
+
+
+CREATE TABLE orders
+(
+    order_id int(11) NOT NULL,
+    username varchar(255) NOT NULL,
+    quantity_ordered int(10) NOT NULL,
+    total_price double NOT NULL,
+    order_date datetime NOT NULL,
+    shipping_date datetime NOT NULL,
+    date_of_arrival datetime NOT NULL,
+    status varchar(15) NOT NULL,
+    comments text,
+    PRIMARY KEY (order_id, username),
+    FOREIGN KEY (order_id) REFERENCES orderItems (order_id) ON UPDATE CASCADE,
+    FOREIGN KEY (username) REFERENCES users (username) ON UPDATE CASCADE
+);
+
+
---
+++ src/main/java/softwareProject/sql/software_project_dataset.sql
@@ -0,0 +1,4 @@
+INSERT INTO `users` (`username`, `email`, `password`, `address`, `isAdmin`)
+VALUES ('admin', 'admin@gmail.com', 'admin', '78 Wheaton Hall Dublin Road, Co.Louth', true),
+       ('Andrew', 'andrew@gmail.com', 'password123@', '88 Wheaton Hall Dublin Road, Co.Louth', false);
+
