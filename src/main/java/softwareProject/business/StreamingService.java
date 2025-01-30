0d99278565d9bbb0927b4a6f2e860ca434d09642
Andrew <Andrewcunningham9911@hotmail.com>
2025-01-30 23:10:27 +0000

    Movie and streaming service added

---
+++ src/main/java/softwareProject/business/StreamingService.java
@@ -0,0 +1,20 @@
+package softwareProject.business;
+
+import lombok.*;
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StreamingService {
+
+    @EqualsAndHashCode.Include
+    private int streamingServiceId;
+    private String movieId;
+    private String movieServiceName;
+    private String movieServiceLink;
+    private double cost;
+
+}
