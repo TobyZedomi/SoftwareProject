0f8d537206ea34e329658ab9377aabdb8265d4cb
jamzy75 <jamzymup@gmail.com>
2025-02-01 20:00:58 +0000

    Added a new method called getAllMovies

--- src/main/java/softwareProject/persistence/MovieDao.java
+++ src/main/java/softwareProject/persistence/MovieDao.java
@@ -3,8 +3,10 @@ package softwareProject.persistence;
 import softwareProject.business.Movie;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface MovieDao {
 
     public ArrayList<Movie> findMovieByGenre(int genre);
+    List<Movie> getAllMovies();
 }
