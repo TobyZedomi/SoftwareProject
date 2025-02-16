7c6a4c4ff8199f6a86db507c701cc1860cdca469
TobyZedomi <tobyzedo4@gmail.com>
2025-02-16 09:55:10 +0000

    putting get most popular movies method in index controller along with videos for movies and  putting all genres for movies and selecting movies by genre in movie index controller along with videos for movies

--- src/main/java/softwareProject/controller/IndexController.java
+++ src/main/java/softwareProject/controller/IndexController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import softwareProject.business.GenreTest;
 import softwareProject.business.MovieTest;
+import softwareProject.business.MovieTrailer;
 import softwareProject.service.MovieService;
 
 import java.util.List;
@@ -33,12 +36,33 @@ public class IndexController {
         List<MovieTest> movies = movieService.getMovies();
         model.addAttribute("movies", movies);
 
+        for (int i = 0; i < movies.size();i++) {
+
+            List<MovieTrailer> trailers = movieService.getTrailer(movies.get(i).getId());
+            model.addAttribute("trailers", trailers);
+        }
+
 
         return "index";
     }
 
     @GetMapping("/movie_index")
-    public String movieIndex(){
+    public String movieIndex(Model model){
+
+
+        List<GenreTest> genres = movieService.getGenres();
+        model.addAttribute("genres", genres);
+
+        List<MovieTest> movieByGenres = movieService.getMoviesByGenre("878");
+        model.addAttribute("movieByGenres", movieByGenres);
+
+
+        for (int i = 0; i < movieByGenres.size();i++) {
+
+            List<MovieTrailer> trailers = movieService.getTrailer(movieByGenres.get(i).getId());
+            model.addAttribute("trailers", trailers);
+        }
+
         return "movie_index";
     }
 
