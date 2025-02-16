604f619f0f9c6ac1c02600740d6593e50cb0cbca
TobyZedomi <tobyzedo4@gmail.com>
2025-02-16 09:40:36 +0000

    method to get all genres for movies and select movies by genre

--- src/main/java/softwareProject/service/MovieService.java
+++ src/main/java/softwareProject/service/MovieService.java
@@ -8,8 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import org.springframework.web.client.RestTemplate;
-import softwareProject.business.MovieResponse;
-import softwareProject.business.MovieTest;
+import softwareProject.business.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -38,4 +37,43 @@ public class MovieService {
 
         return response.getBody().getResults();
     }
+
+
+    // get trailer based on movie id
+
+    public List<MovieTrailer> getTrailer(int id){
+
+        ResponseEntity<MovieTrailerResponse> response = restTemplate.getForEntity("https://api.themoviedb.org/3/movie/"+id+"/videos?api_key="+API_KEY, MovieTrailerResponse.class);
+
+
+        return response.getBody().getResults();
+    }
+
+
+    // get genre of movies
+
+
+    private final String API_URL1 = "https://api.themoviedb.org/3/genre/movie/list?api_key=";
+
+
+    public List<GenreTest> getGenres(){
+
+        ResponseEntity<GenreResponse> response = restTemplate.getForEntity(API_URL1+API_KEY, GenreResponse.class);
+
+
+        return response.getBody().getGenres();
+
+    }
+
+
+    // get movie by genre id
+
+    public List<MovieTest> getMoviesByGenre(String genre_id){
+
+        ResponseEntity<MovieResponse> response = restTemplate.getForEntity("https://api.themoviedb.org/3/discover/movie?with_genres="+genre_id+"&api_key="+API_KEY, MovieResponse.class);
+
+
+        return response.getBody().getResults();
+    }
+
 }
