7287fffee30647bb2a1bc6314ba89c5024ee5982
Andrew <Andrewcunningham9911@hotmail.com>
2025-02-17 23:42:07 +0000

    Friend class added

---
+++ src/main/java/softwareProject/business/Friends.java
@@ -0,0 +1,17 @@
+package softwareProject.business;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Friends {
+
+    private String friend1;
+    private String friend2;
+    private boolean request;
+}
