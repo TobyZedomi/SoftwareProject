ef8f1b9b3ff6716313e2f5b8b02be6385cced71a
Andrew <Andrewcunningham9911@hotmail.com>
2025-02-24 22:50:34 +0000

    Friend dao updated

--- src/main/java/softwareProject/persistence/FriendDao.java
+++ src/main/java/softwareProject/persistence/FriendDao.java
@@ -1,10 +1,17 @@
 package softwareProject.persistence;
 
+import softwareProject.business.Friends;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public interface FriendDao {
 
     public int insertIntoFriend(String user,String friend);
     public int acceptRequest(String user,String friend);
     public int declineRequest(String user,String friend);
-    public int getAllRequests(String user);
-    public int getAllFriends(String user);
+    public ArrayList<Friends> getAllRequests(String user);
+    public int getNumberOfFriends(String user);
+    public boolean getAFriend(String user, String friend);
+    public ArrayList<Friends> getAllFriends(String user);
 }
--- src/main/java/softwareProject/persistence/FriendDaoImpl.java
+++ src/main/java/softwareProject/persistence/FriendDaoImpl.java
@@ -1,8 +1,15 @@
 package softwareProject.persistence;
 
+import org.springframework.context.annotation.ScopeMetadata;
+import softwareProject.business.Friends;
+import softwareProject.business.Genre;
+import softwareProject.business.Movie;
 import softwareProject.business.User;
 
 import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FriendDaoImpl extends MySQLDao implements FriendDao{
 
@@ -38,7 +45,7 @@ public class FriendDaoImpl extends MySQLDao implements FriendDao{
         // to use the ? notation (so you can fill in the blanks later)
         try(PreparedStatement ps = conn.prepareStatement("insert into friends values(?, ?,false)")) {
             // Fill in the blanks, i.e. parameterize the update
-            ps.setString(1, user);
+            ps.setString(1, user );
             ps.setString(2, friend);
 
             // Execute the update and store how many rows were affected/changed
@@ -74,10 +81,10 @@ public class FriendDaoImpl extends MySQLDao implements FriendDao{
         // TRY to prepare a statement from the connection
         // When you are parameterizing the update, remember that you need
         // to use the ? notation (so you can fill in the blanks later)
-        try(PreparedStatement ps = conn.prepareStatement("update friends set status = true where friend1 = ? and friend2 = ?)")) {
+        try(PreparedStatement ps = conn.prepareStatement("update friends set request = true where friend1 = ? and friend2 = ?")) {
             // Fill in the blanks, i.e. parameterize the update
-            ps.setString(1, user);
-            ps.setString(2, friend);
+            ps.setString(1, friend);
+            ps.setString(2, user);
 
             // Execute the update and store how many rows were affected/changed
             // when inserting, this number indicates if the row was
@@ -112,10 +119,10 @@ public class FriendDaoImpl extends MySQLDao implements FriendDao{
         // TRY to prepare a statement from the connection
         // When you are parameterizing the update, remember that you need
         // to use the ? notation (so you can fill in the blanks later)
-        try(PreparedStatement ps = conn.prepareStatement("delete from friends where friend1 = ? and friend2 = ? and status = false)")) {
+        try(PreparedStatement ps = conn.prepareStatement("delete from friends where friend1 = ? and friend2 = ? and request = false")) {
             // Fill in the blanks, i.e. parameterize the update
-            ps.setString(1, user);
-            ps.setString(2, friend);
+            ps.setString(1, friend);
+            ps.setString(2, user);
 
             // Execute the update and store how many rows were affected/changed
             // when inserting, this number indicates if the row was
@@ -135,51 +142,80 @@ public class FriendDaoImpl extends MySQLDao implements FriendDao{
 
         return rowsAffected;
     }
+
     @Override
-    public int getAllRequests(String user){
-        // DATABASE CODE
-        //
-        // Create variable to hold the result of the operation
-        // Remember, where you are NOT doing a select, you will only ever get
-        // a number indicating how many things were changed/affected
-        int rowsAffected = 0;
+    public ArrayList<Friends> getAllRequests(String user){
 
+        ArrayList<Friends> requests = new ArrayList<>();
 
+        // Get a connection using the superclass
         Connection conn = super.getConnection();
-
-        // TRY to prepare a statement from the connection
-        // When you are parameterizing the update, remember that you need
+        // TRY to get a statement from the connection
+        // When you are parameterizing the query, remember that you need
         // to use the ? notation (so you can fill in the blanks later)
-        try(PreparedStatement ps = conn.prepareStatement("select friend1 from friends where friend2 = ? and status = false)")) {
-            // Fill in the blanks, i.e. parameterize the update
+        try (PreparedStatement ps = conn.prepareStatement("select * from friends where friend2 = ? and request = false")) {
+
+            // Fill in the blanks, i.e. parameterize the query
             ps.setString(1, user);
 
-            // Execute the update and store how many rows were affected/changed
-            // when inserting, this number indicates if the row was
-            // added to the database (>0 means it was added)
-            rowsAffected = ps.executeUpdate();
-        }// Add an extra exception handling block for where there is already an entry
-        // with the primary key specified
-        catch (SQLIntegrityConstraintViolationException e) {
-            System.out.println("Constraint Exception occurred: " + e.getMessage());
-            // Set the rowsAffected to -1, this can be used as a flag for the display section
-            rowsAffected = -1;
-        }catch(SQLException e){
-            System.out.println("SQL Exception occurred when attempting to prepare/execute SQL");
+
+            // TRY to execute the query
+            try (ResultSet rs = ps.executeQuery()) {
+                // Extract the information from the result set
+                // Use extraction method to avoid code repetition!
+                while(rs.next()){
+
+                    Friends m = mapRow(rs);
+                    requests.add(m);
+                }
+            } catch (SQLException e) {
+                System.out.println("SQL Exception occurred when executing SQL or processing results.");
+                System.out.println("Error: " + e.getMessage());
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL Exception occurred when attempting to prepare SQL for execution");
             System.out.println("Error: " + e.getMessage());
             e.printStackTrace();
+        }finally {
+            // Close the connection using the superclass method
+            super.freeConnection(conn);
         }
-
-        return rowsAffected;
+        return requests;
     }
     @Override
-    public int getAllFriends(String user){
+    public int getNumberOfFriends(String user){
+
+        Connection conn = super.getConnection();
+
+        try(PreparedStatement ps = conn.prepareStatement("Select count(*) from friends where friend1 = ? and request = true")){
+            try(ResultSet rs = ps.executeQuery()){
+                if(rs.next()){
+                    return rs.getInt(1);
+                }
+            }catch(SQLException e){
+                System.out.println(LocalDateTime.now() + ": An SQLException  occurred while running the query" +
+                        " or processing the result.");
+                System.out.println("Error: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }catch(SQLException e){
+            System.out.println(LocalDateTime.now() + ": An SQLException  occurred while preparing the SQL " +
+                    "statement.");
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
+    @Override
+    public boolean getAFriend(String user, String friend){
         // DATABASE CODE
         //
         // Create variable to hold the result of the operation
         // Remember, where you are NOT doing a select, you will only ever get
         // a number indicating how many things were changed/affected
-        int rowsAffected = 0;
+        boolean complete = false;
 
 
         Connection conn = super.getConnection();
@@ -187,27 +223,84 @@ public class FriendDaoImpl extends MySQLDao implements FriendDao{
         // TRY to prepare a statement from the connection
         // When you are parameterizing the update, remember that you need
         // to use the ? notation (so you can fill in the blanks later)
-        try(PreparedStatement ps = conn.prepareStatement("Select count(*) from friends where friend1 = ? and status = true)")) {
+        try(PreparedStatement ps = conn.prepareStatement("Select * from friends where (friend1 = ? and friend2 = ? or friend2 = ? and friend1 = ?)")) {
             // Fill in the blanks, i.e. parameterize the update
             ps.setString(1, user);
-
+            ps.setString(2,friend);
+            ps.setString(3, user);
+            ps.setString(4,friend);
             // Execute the update and store how many rows were affected/changed
             // when inserting, this number indicates if the row was
             // added to the database (>0 means it was added)
-            rowsAffected = ps.executeUpdate();
+            try(ResultSet rs = ps.executeQuery()){
+                while(rs.next()){
+                    complete = true;
+                }
+            }
         }// Add an extra exception handling block for where there is already an entry
         // with the primary key specified
         catch (SQLIntegrityConstraintViolationException e) {
             System.out.println("Constraint Exception occurred: " + e.getMessage());
             // Set the rowsAffected to -1, this can be used as a flag for the display section
-            rowsAffected = -1;
+            complete = false;
         }catch(SQLException e){
             System.out.println("SQL Exception occurred when attempting to prepare/execute SQL");
             System.out.println("Error: " + e.getMessage());
             e.printStackTrace();
         }
 
-        return rowsAffected;
+        return complete;
+    }
+    @Override
+    public ArrayList<Friends> getAllFriends(String user){
+
+        ArrayList<Friends> requests = new ArrayList<>();
+
+        // Get a connection using the superclass
+        Connection conn = super.getConnection();
+        // TRY to get a statement from the connection
+        // When you are parameterizing the query, remember that you need
+        // to use the ? notation (so you can fill in the blanks later)
+        try (PreparedStatement ps = conn.prepareStatement("Select * from friends where (friend1 = ? or friend2 = ?) and request = true")) {
+
+            // Fill in the blanks, i.e. parameterize the query
+            ps.setString(1, user);
+            ps.setString(2, user);
+
+
+            // TRY to execute the query
+            try (ResultSet rs = ps.executeQuery()) {
+                // Extract the information from the result set
+                // Use extraction method to avoid code repetition!
+                while(rs.next()){
+
+                    Friends m = mapRow(rs);
+                    requests.add(m);
+                }
+            } catch (SQLException e) {
+                System.out.println("SQL Exception occurred when executing SQL or processing results.");
+                System.out.println("Error: " + e.getMessage());
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL Exception occurred when attempting to prepare SQL for execution");
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }finally {
+            // Close the connection using the superclass method
+            super.freeConnection(conn);
+        }
+        return requests;
+    }
+
+    private Friends mapRow(ResultSet rs)throws SQLException {
+
+        Friends f = new Friends(
+                rs.getString("friend1"),
+                rs.getString("friend2"),
+                rs.getBoolean("request")
+        );
+        return f;
     }
 
 }
