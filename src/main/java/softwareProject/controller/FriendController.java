b9b8628008d28b291ca0c165de9c96b4bfb40a7e
Andrew <Andrewcunningham9911@hotmail.com>
2025-03-05 14:25:52 +0000

    Friend controller updated to get the number of friends each user has

--- src/main/java/softwareProject/controller/FriendController.java
+++ src/main/java/softwareProject/controller/FriendController.java
@@ -1,41 +1,101 @@
 package softwareProject.controller;
 
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import softwareProject.business.Friends;
 import softwareProject.business.User;
 import softwareProject.persistence.FriendDao;
 import softwareProject.persistence.FriendDaoImpl;
 import softwareProject.persistence.UserDao;
 import softwareProject.persistence.UserDaoImpl;
-@Controller
 
+import java.util.ArrayList;
+
+@Slf4j
+@Controller
 public class FriendController {
-    @PostMapping("/addFriend")
-    public String addFriend(HttpSession session, @RequestParam(name = "friendUsername") String friendUsername, Model model) {
+
+    public static int getNumberOfFriends(HttpSession session, @RequestParam(name = "friendUsername") String friendUsername, Model model){
+
+        FriendDao friendDao = new FriendDaoImpl("database.properties");
+
+        int numberOfFriends = friendDao.getNumberOfFriends(friendUsername);
+
+        model.addAttribute("numberOfFriends", numberOfFriends);
+        log.info("Number of friends: " + numberOfFriends);
+
+        return numberOfFriends;
+    }
+
+    public static ArrayList<Friends> getAllFriends(HttpSession session, @RequestParam(name = "friendUsername") String friendUsername, Model model){
+
+        FriendDao friendDao = new FriendDaoImpl("database.properties");
+
+        ArrayList<Friends> friends = friendDao.getAllFriends(friendUsername);
+
+        log.info("Number of friends: " + friends.size());
+
+        return friends;
+    }
+
+    @GetMapping("/searchForYourFriends")
+    public String searchForYourFriend(HttpSession session,@RequestParam(name= "friendUsername") String friendUsername, Model model){
         User loggedInUser = (User) session.getAttribute("loggedInUser");
 
-        UserDao userDao = new UserDaoImpl("database.properties");
         FriendDao friendDao = new FriendDaoImpl("database.properties");
-        User friend = userDao.findUserByUsername(friendUsername);
+        UserDao userDao = new UserDaoImpl("database.properties");
 
-        if(friendDao.getAFriend(loggedInUser.getUsername(),friendUsername)){
-            model.addAttribute("error","You have already sent a request to this user or are friends with them");
-            return "friends";
+        ArrayList<Friends> friends = friendDao.searchForFriends(loggedInUser.getUsername(),friendUsername);
+        ArrayList<Friends> allFriends = new ArrayList<>();
+        ArrayList<User> details = new ArrayList<>();
+
+        if(friends.isEmpty()){
+            model.addAttribute("Empty", "Sorry you have no friends with this username");
         }
 
-        if (friend != null) {
-
-            int result = friendDao.insertIntoFriend(loggedInUser.getUsername(), friendUsername);
-
-            if (result > 0) {
-                model.addAttribute("success", "Friend request has been sent");
-            } else{
-                model.addAttribute("error", "Unable to send friend request. Please try again");
+        for(int i = 0; i< friends.size();i++){
+            String name;
+            if (friends.get(i).getFriend1().equals(loggedInUser.getUsername())) {
+                name = friends.get(i).getFriend2();
+            } else {
+                name = friends.get(i).getFriend1();
+            }
+            User friendUser = userDao.findUserByUsername(name);
+            allFriends = getAllFriends(session,friendUser.getUsername(),model);
+            if(name != null){
+                details.add(friendUser);
             }
         }
+
+        model.addAttribute("total", allFriends.size());
+        model.addAttribute("friends", details);
+
         return "friends";
     }
+
+    @PostMapping("/deleteAFriend")
+    public String deleteAFriend(HttpSession session,@RequestParam(name= "friendUsername") String friendUsername, Model model){
+        User loggedInUser = (User) session.getAttribute("loggedInUser");
+
+        FriendDao friendDao = new FriendDaoImpl("database.properties");
+
+        int deleted = friendDao.deleteAFriend(loggedInUser.getUsername(),friendUsername);
+
+        if(deleted > 0 ){
+            model.addAttribute("deleted", "You are no longer friends with "+friendUsername);
+            log.info("You are no longer friends with "+friendUsername);
+        }else{
+            model.addAttribute("failedDelete","Failed to remove "+friendUsername);
+            log.info("Failed to remove "+friendUsername);
+        }
+
+        return "friends";
+    }
+
+
 }
