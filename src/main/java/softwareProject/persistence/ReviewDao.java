bb20d30483ed6276d05b4e3389afc3aac9233781
jamzy75 <jamzymup@gmail.com>
2025-03-11 00:28:20 +0000

    Review dao methods

--- src/main/java/softwareProject/persistence/ReviewDao.java
+++ src/main/java/softwareProject/persistence/ReviewDao.java
@@ -5,5 +5,8 @@ import java.util.List;
 
 public interface ReviewDao {
     void saveReview(Review review);
+    int updateReview(Review review);
+    int deleteReview(String username, int movieId);
     List<Review> getAllReviews();
+    List<Review> getReviewsByUser(String username); // Fetch reviews by username
 }
