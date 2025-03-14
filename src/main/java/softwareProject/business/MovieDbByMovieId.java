dd009d708364697014d65c2f589d9f8206fd175f
TobyZedomi <tobyzedo4@gmail.com>
2025-03-14 07:33:05 +0000

    Creating the page to get movie by id and the service to get the movie information for said movie, so when the user clicks on the video to watch for the movie it says the movie name

---
+++ src/main/java/softwareProject/business/MovieDbByMovieId.java
@@ -0,0 +1,16 @@
+package softwareProject.business;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MovieDbByMovieId {
+
+
+    private String title;
+}
