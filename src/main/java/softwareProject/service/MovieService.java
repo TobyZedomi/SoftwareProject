f05929df806a579edd7fcb5c3eab88518dba2012
jamzy75 <jamzymup@gmail.com>
2025-04-22 02:00:25 +0100

    added a method that gets reviews by movie id and another that get reviews for popular movies

--- src/main/java/softwareProject/service/MovieService.java
+++ src/main/java/softwareProject/service/MovieService.java
@@ -11,7 +11,9 @@ import org.springframework.web.client.RestTemplate;
 import softwareProject.business.*;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.fasterxml.jackson.databind.cfg.CoercionInputShape.Array;
 
@@ -144,6 +146,26 @@ public class MovieService {
         ResponseEntity<MovieSearchByGenreResponse> response = restTemplate.getForEntity("https://api.themoviedb.org/3/discover/movie?certification="+query+"&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres="+genreId+"&api_key="+API_KEY, MovieSearchByGenreResponse.class);
 
         return response.getBody().getResults();
+    }
+
+    public List<MovieReview> getReviewsByMovieId(int movieId) {
+        String url = "https://api.themoviedb.org/3/movie/" + movieId + "/reviews?api_key=" + API_KEY;
+        ResponseEntity<MovieReviewResponse> response = restTemplate.getForEntity(url, MovieReviewResponse.class);
+        return response.getBody().getResults();
+    }
+
+    public Map<String, List<MovieReview>> getReviewsForPopularMovies() {
+        List<MovieTest> popularMovies = getMovies();
+        Map<String, List<MovieReview>> reviewsByMovie = new HashMap<>();
+
+        for (MovieTest movie : popularMovies) {
+            List<MovieReview> reviews = getReviewsByMovieId(movie.getId());
+            if (!reviews.isEmpty()) {
+                reviewsByMovie.put(movie.getTitle(), reviews);
+            }
+        }
+
+        return reviewsByMovie;
     }
 
 }
